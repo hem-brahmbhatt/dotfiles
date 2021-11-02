e2b3864d9683cd708801bad78eee8527e914194f
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2021-11-02 15:00:28 +0000

    added kitty terminal config

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 dotfiles=".alias .bash_profile .bashrc .commands .git-completion.bash .gitconfig .mac_colors .profile .sensible.tmux .tmux.conf .tmux.conf.local"
+config="nvim kitty"
 
 continuePrompt() {
   printf "Continue (Y/n)?"
@@ -17,6 +18,13 @@ installDotfiles() {
   done
 }
 
+installConfig() {
+  for configFolder in ${config}; do
+    (cd ~/.config && ln -s ~/Projects/dotfiles/${configFolder} || continuePrompt)
+  done
+}
+
 echo "Installing dotfiles..."
 installDotfiles
+installConfig
 echo "Done!"
