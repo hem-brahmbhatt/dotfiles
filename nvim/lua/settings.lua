e0a0f8bb03cef043355ba817c3c1b1162c4f5ae4
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2021-12-01 10:50:03 +0000

    fixed undo history in neovim

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,7 +2,7 @@
 vim.g.mapleader = ','
 
 -- Save undos to disk
-vim.api.nvim_buf_set_option(0, 'undofile', true)
+-- vim.api.nvim_buf_set_option(0, 'undofile', true)
 -- vim.bo.undofile = true
 
 -- nvim-compe
@@ -17,6 +17,7 @@ cmd 'syntax enable'
 cmd 'filetype plugin indent on'
 cmd 'set number'  -- show line numbers
 cmd 'set nomodeline'
+cmd 'set undofile'  -- Save undos to disk
 
 utils.opt('b', 'shiftwidth', indent)                -- Use indents of 4 spaces
 utils.opt('b', 'expandtab', true)                   -- Tabs are spaces, not tabs
