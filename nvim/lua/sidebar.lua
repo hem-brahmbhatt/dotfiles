b6593c9f9ebc658a0655879a90e7803b195691cc
hbrahmbhatt <hbrahmbhatt@bloomberg.net>
2022-10-22 18:31:39 +0100

    ignoring buffers not within current working directory

--- nvim/lua/sidebar.lua
+++ nvim/lua/sidebar.lua
@@ -1,6 +1,18 @@
 local sidebar = require("sidebar-nvim")
 
-sidebar.setup()
+local currentPath = vim.loop.cwd()
+
+sidebar.setup({
+    sections = { "git", "diagnostics", "buffers", "todos" },
+    buffers = {
+        ignored_buffers = {
+            "^(?!"..string.gsub(currentPath, "/","\\/")..").+"
+        }
+    },
+    update_interval = 10000
+})
+
+
 
 vim.keymap.set("n", "<C-s>", function()
   sidebar.toggle()
